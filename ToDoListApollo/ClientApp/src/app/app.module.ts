c4d490c921b7e4af4969b90f879dbe6a91cab129
CharlesSamamaRochegude <charles.samamarochegude@viacesi.fr>
2022-10-19 17:31:26 +0200

    src/app
    Ajout des différentes classes

--- ToDoListApollo/ClientApp/src/app/app.module.ts
+++ ToDoListApollo/ClientApp/src/app/app.module.ts
@@ -10,15 +10,24 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { TacheComponent } from './tache/tache.component';
+import { ListeComponent } from './liste/liste.component';
+import { MainComponent } from './main/main.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    TacheComponent,
+    ListeComponent,
+    MainComponent,
+    UtilisateurComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
