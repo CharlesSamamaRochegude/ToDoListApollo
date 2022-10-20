98ea2a23ba60687d78192e2e071a4b647e66b04b
raclettedesavoie <alexis.riviere@outlook.fr>
2022-10-20 10:37:21 +0200

    pack

--- ToDoListApollo/ClientApp/src/app/liste/liste.component.ts
+++ ToDoListApollo/ClientApp/src/app/liste/liste.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../list';
 import { LISTS } from '../mock_list';
+
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.component.html',
   styleUrls: ['./liste.component.css']
 })
+
 export class ListeComponent implements OnInit {
   lists = LISTS;
+  
   constructor() { }
 
   ngOnInit(): void {
--- ToDoListApollo/ClientApp/src/app/mock_list.ts
+++ ToDoListApollo/ClientApp/src/app/mock_list.ts
@@ -4,5 +4,5 @@ export const LISTS: List[] = [
   { id: 12, name: 'manger' },
   { id: 13, name: 'boire' },
   { id: 14, name: 'develloper' },
-  { id: 15, name: 'dormir' },
+  { id: 15, name: 'dormir' }
 ];
