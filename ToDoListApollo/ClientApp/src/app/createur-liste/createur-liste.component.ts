501f398e0013776bb713f962eca073a4b80f74f3
CharlesSamamaRochegude <charles.samamarochegude@viacesi.fr>
2022-10-25 09:39:23 +0200

    src/app/createur-liste
    Modification mineure pour récupération des données et formattage de la date

--- ToDoListApollo/ClientApp/src/app/createur-liste/createur-liste.component.ts
+++ ToDoListApollo/ClientApp/src/app/createur-liste/createur-liste.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-createur-liste',
@@ -9,23 +11,25 @@ import { DatePipe } from '@angular/common';
 export class CreateurListeComponent implements OnInit {
   titre?: string;
   description?: string;
-  date?: string;
+  date?: string | null;
 
   aujourdhui: string | null = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
 
 
-  constructor(private datePipe: DatePipe) {
+  constructor(private datePipe: DatePipe, private location: Location) {
   }
 
   ngOnInit(): void {
+    this.date = this.aujourdhui;
   }
 
-  onSubmitForm() {
+  onSubmitForm(): void {
     if (this.date) {
       this.date = this.date.split("-")[2] + "-" + this.date.split("-")[1] + "-" + this.date.split("-")[0];
     }
     console.log(this.titre);
     console.log(this.date);
+    this.location.back();
   }
 
 }
