b88b84fdcc38220a0035acad03152a42ba31da72
CharlesSamamaRochegude <charles.samamarochegude@viacesi.fr>
2022-10-27 10:39:06 +0200

    src/app
    Renommage des attributs de tache

--- ToDoListApollo/ClientApp/src/app/tache.ts
+++ ToDoListApollo/ClientApp/src/app/tache.ts
@@ -3,10 +3,10 @@ import { personne } from "./personne";
 
 export interface tache {
   id_t: number;
-  Titre_t: string;
+  titre_t: string;
   active_l: boolean;
-  Date_echeance_l: string | null;
-  TodoListId: number;
-  PersonneId: number;
+  date_echeance_l: string | null;
+  todoListId: number;
+  personneId: number;
 
 }
