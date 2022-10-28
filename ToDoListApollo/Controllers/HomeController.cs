1492c373aecafb659bd38615abbffb0018ae8528
CharlesSamamaRochegude <charles.samamarochegude@viacesi.fr>
2022-10-28 16:27:02 +0200

    Divers/
    Corrections mineures liées au changement de verson d'EntityFramework

--- ToDoListApollo/Controllers/HomeController.cs
+++ ToDoListApollo/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Data.Entity;
 using System.Linq.Expressions;
 using System.Net;
 using System.Reflection;
@@ -223,8 +223,8 @@ namespace ToDoListApollo.Controllers
         //Affichage de l'ensemble des ToDoListes
         [HttpGet("list")]
         public IEnumerable<ToDoListe> GetToDoListes() {
-            var resu = _context.ToDoListe.Include(t => t.Personne).ToList();
-            return _context.ToDoListe.Include(p => p.Personne).ToList();
+            //var resu = _context.ToDoListe.Include(t => t.Personne).ToList();
+            return _context.ToDoListe/*.Include(p => p.Personne)*/.ToList();
         }
 
         //Affichage de l'ensemble des ToDoListes en asynchrone
